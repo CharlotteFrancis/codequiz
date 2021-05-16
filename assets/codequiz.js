8fa1059469002a21d9d823e4a2f138b5b2adb248
Charlotte Wong <ChiharuTaru@gmail.com>
2021-05-16 13:02:02 -0700

    added end screen and timer

--- assets/codequiz.js
+++ assets/codequiz.js
@@ -3,6 +3,10 @@
 let score = 0;
 let count = 0;
 
+//timer stuff
+let time = 30;
+let timer
+
 //array of the questions
 let questions = [
     {
@@ -52,27 +56,71 @@ let nextQuestion = _ =>{
 }
 //endScreen
 let endScreen = _ =>{
-    
+    clearInterval(timer) //stop timer
+    document.getElementById('timer').classList = 'my-timer hidden' //hide timer
+    document.getElementById('question').innerHTML = '' //reset question section
+    let end = document.createElement('div')
+    end.classList = "centered"
+    end.innerHTML = `
+    <h3>Finshed!<h3>
+    <h6>Score: ${score}<h6>
+    <div class="row">
+        <form>
+            <div class="col s4"></div>
+            <div class="input-field col s4">
+                <input class = "initials-color" value="" id="initials">
+                <label class="active my-label" for="initials">Enter your initials to submit!</label>
+                <button id = "submit" class="btn waves-effect waves-light red" type="submit" name="action">Submit</button>
+            </div>
+            <div class="col s4"></div>
+        </form>
+    </div>
+    `
+    document.getElementById('endscreen').append(end) //append endscreen
 }
+//showScores
+let showScores = _ =>{
+    document.getElementById('question').innerHTML = '' //reset question section
+    document.getElementById('endscreen').innerHTML = '' //reset end section
 
+    //display score table, retrieve from  local memory
+
+}
 //start quiz
 document.getElementById('letsgo').addEventListener('click', _ => {
     document.getElementById('letsgo').remove()
-    console.log('lets go clicked')
+    document.getElementById('timer').innerHTML = `Time Left: ${time}`
+    document.getElementById('timer').classList = 'my-timer'
+    timer = setInterval(() =>{
+        if(time > 0){ //if out of time then stop the game
+            time--
+            document.getElementById('timer').innerHTML = `Time Left: ${time}`
+        }else{
+            endScreen()
+        }     
+    },1000)
     nextQuestion();
 })
-
+//listen for answer selection
 document.addEventListener('click', event =>{
     event.preventDefault()
     if(event.target.classList.contains('choice')){
         if(event.target.dataset.value === questions[count].answer){
             score++  
+        }else{
+            time-=5
         }
         count++
+        //go to end screen if out of questions
+        if(count < questions.length){
+            nextQuestion()
+        }else{
+            endScreen()
+        }
     }
-    if(count < questions.length){
-        nextQuestion()
-    }else{
-        //end screen here
-    }
+    
+})
+//listen for highscores
+document.getElementById('submit').addEventListener('click', _ => {
+    //display scoreboard
 })
