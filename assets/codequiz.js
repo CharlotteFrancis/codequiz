2c0f3af5fd6f82eaf818bd98cbc1b370df2b5401
Charlotte Wong <ChiharuTaru@gmail.com>
2021-05-16 13:52:27 -0700

    updated but the button is broken now

--- assets/codequiz.js
+++ assets/codequiz.js
@@ -68,9 +68,9 @@ let endScreen = _ =>{
         <form>
             <div class="col s4"></div>
             <div class="input-field col s4">
-                <input class = "initials-color" value="" id="initials">
+                <input class="initials-color" value="" id="initials">
                 <label class="active my-label" for="initials">Enter your initials to submit!</label>
-                <button id = "submit" class="btn waves-effect waves-light red" type="submit" name="action">Submit</button>
+                <button id="submit" class="btn waves-effect waves-light red" type="submit" name="action">Submit</button>
             </div>
             <div class="col s4"></div>
         </form>
@@ -82,13 +82,39 @@ let endScreen = _ =>{
 let showScores = _ =>{
     document.getElementById('question').innerHTML = '' //reset question section
     document.getElementById('endscreen').innerHTML = '' //reset end section
-
     //display score table, retrieve from  local memory
-
 }
+//sortScores
+let sortScores(a, b){
+    if(a[1] === b[1]){
+        return 0; //score equal
+    }else{
+        if(a[1] < b[1]){
+            return -1 
+        }else{
+            return 1
+        }
+    }
+}
+//submitScores
+let submitScores = _ =>{
+    let initials = document.getElementById('initials').value
+    //define scores array from local storage, if it doesnt exist then create empty array
+    let scores = JSON.parse(localStorage.getItem('scores')) || []
+    //add initials and score to scores array
+    scores.push({
+        initials,
+        score
+    })
+    scores.sort(sortScores)
+    console.log(scores) // log log log
+    localStorage.setItem('scores', JSON.stringify(scores))
+}
+
 //start quiz
 document.getElementById('letsgo').addEventListener('click', _ => {
     document.getElementById('letsgo').remove()
+    console.log('lets go pressed')
     document.getElementById('timer').innerHTML = `Time Left: ${time}`
     document.getElementById('timer').classList = 'my-timer'
     timer = setInterval(() =>{
@@ -121,6 +147,14 @@ document.addEventListener('click', event =>{
     
 })
 //listen for highscores
+document.getElementById('highscore-btn').addEventListener('click', _ => {
+    //display scoreboard
+    clearInterval(timer)
+    showScores()
+})
+//listen for score submition
 document.getElementById('submit').addEventListener('click', _ => {
     //display scoreboard
+    submitScores()
+    showScores()
 })
